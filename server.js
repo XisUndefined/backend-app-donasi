1e9b82ffe1eb12d7860f3ee35e322828376385ff
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-02-12 22:57:36 +0700

    adding catch error in database connection

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import "dotenv/config";
 import app from "./app.js";
 import mongoose from "mongoose";
 
-// console.log(process.env);
-
 mongoose
   .connect(process.env.DB_LOCAL_CONN_STR, { dbName: process.env.DB_NAME })
-  .then(() => console.log("Database Connection Successful."));
+  .then(() => console.log("Database Connection Successful."))
+  .catch((error) => console.error("Database Connection Error.", error));
 
 const port = process.env.PORT || 3000;
 
